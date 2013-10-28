f0a7aca3750eb89855e1d5b388cc0b37a23888a6
Maurício Gardini <mauriciogardini@gmail.com>
2013-10-28 14:00:48 -0200

    Added the solution to the problem 0001.

---
+++ problem_0001.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+If we list all the natural numbers below 10 that are multiples of 3 or 5,
+we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 1000.
+"""
+
+import sys
+import unittest
+
+class Problem0001():
+    def sum_multiples_below_n(self, n):
+        multiples_sum = 0
+        for number in range(1, n):
+            if number % 3 == 0 or number % 5 == 0:
+                multiples_sum += number
+        return multiples_sum
+
+    def run(self):
+        print 'The sum of all the multiples of 3 and 5 below 1000 equals %i' %\
+            (self.sum_multiples_below_n(1000))
+
+class TestSumMultiples(unittest.TestCase):
+    def setUp(self):
+        self.problem_0001 = Problem0001()
+
+    def test_sum_10(self):
+        self.assertEqual(self.problem_0001.sum_multiples_below_n(10), 23)
+
+if __name__ == '__main__':
+    if 'test' in sys.argv:
+        sys.argv.remove('test')
+        unittest.main()
+    else:
+        problem0001 = Problem0001()
+        problem0001.run()
