514006efd835b31c18f353431523172c8f67bac8
Maurício Gardini <mauriciogardini@gmail.com>
2013-11-01 13:08:42 -0200

    Added timer to all of the problems solved so far.

---
+++ timer.py
@@ -0,0 +1,12 @@
+import time
+
+class Timer(object):
+    def __enter__(self):
+        self.start = time.time()
+        return self
+
+    def __exit__(self, *args):
+        self.end = time.time()
+        self.secs = self.end - self.start
+        self.msecs = self.secs * 1000
+        print 'Elapsed time: %f ms' % self.msecs 
